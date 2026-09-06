5a4088da54761f5f2e773509ddfd7a686d1c0582
sumitsharansatsangi <sharansumitkumar@gmail.com>
2026-09-06 16:15:42 +0530

    agp upgraded

--- android/local-config.gradle.kts
+++ android/local-config.gradle.kts
@@ -2,4 +2,4 @@ extra["compileSdk"] = 37
 extra["minSdk"] = 24
 extra["targetSdk"] = 37
 extra["javaVersion"] = JavaVersion.VERSION_21
-extra["androidGradlePluginVersion"] = "9.2.1"
+extra["androidGradlePluginVersion"] = "9.4.0"
